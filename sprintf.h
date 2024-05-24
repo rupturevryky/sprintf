d858066d2c392894db35e5a18425bfddaa9213e5
Rupturevryky <you@example.com>
2024-05-24 22:15:01 +0300

    comment func

--- sprintf.h
+++ sprintf.h
@@ -5,7 +5,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  //
+#include <string.h>
 
 #endif
 
@@ -14,12 +14,12 @@
 
 #include "write.c"
 
-void add_char(char **s, char ch, int *count);
-int write_int(char **s, va_list *args, int *count);
-int write_char(char **s, va_list *args, int *count);
-void write_float(char **s, va_list *args, int *count);
-int write_scientific_notation(char **s, va_list *args, int *count, char E);
-int write_string(char **s, va_list *args, int *count);
+// void add_char(char **s, char ch, int *count);
+// int write_int(char **s, va_list *args, int *count);
+// int write_char(char **s, va_list *args, int *count);
+// void write_float(char **s, va_list *args, int *count);
+// int write_scientific_notation(char **s, va_list *args, int *count, char E);
+// int write_string(char **s, va_list *args, int *count);
 
 #endif
 
@@ -29,9 +29,9 @@ int write_string(char **s, va_list *args, int *count);
 #include <math.h>
 
 #include "myMath.c"
-int increase_discharge(int exp);
-int getFirstDigit(int number);
-int digit_down(int *num, int *tpm_len, int *len, int E, int *count, char **s,
-               int should_point);
+// int increase_discharge(int exp);
+// int getFirstDigit(int number);
+// int digit_down(int *num, int *tpm_len, int *len, int E, int *count, char **s,
+//                int should_point);
 
 #endif
